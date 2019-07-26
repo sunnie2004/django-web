f7456ea54238587e71e75599574d06b5eb5b184b
sunny <sunnie2004@gmail.com>
2019-07-25 21:49:29 -0500

    delete apps and template,put them under pyshop(v1.3)

--- apps/cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.CartView, name='cart'),
-    path('add', views.CartAddView, name='add'),
-    path('remove/<id>', views.CartRemove, name='remove'),
-    path('update', views.CartUpdate, name='update'),
-]
--- apps/payment/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentConfig(AppConfig):
-    name = 'apps.payment'
-    verbose_name = 'Payment'
-
-    def ready(self):
-        import apps.payment.signals     # import signal handlers
